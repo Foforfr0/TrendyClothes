9e777f37ff12395bcf1621e817972cd8b236f2ca
Foforfr0 <foforfr007@gmail.com>
2025-06-09 15:59:41 -0600

    Wpf

--- Frontend/WPF/Pages/WindowContainer.xaml.cs
+++ Frontend/WPF/Pages/WindowContainer.xaml.cs
@@ -1,17 +1,24 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using WpfApp.Pages.User.Auth;
-using WpfApp.Services.User.Auth;
 
 namespace WpfApp.Pages {
     /// <summary>
     /// Interaction logic for WindowContainer.xaml
     /// </summary>
     public partial class WindowContainer : Window {
+        private static Frame _mainFrame = new Frame ();
+
         public WindowContainer () {
             InitializeComponent ();
 
-            MainFrame.Navigate (new Login ());
+            _mainFrame = MainFrame;
+            _mainFrame.Navigate (new Login ());
             //MainFrame.Navigate (new Login (_loginService));
+        }
+
+        public static void SetFrame (Page page) {
+            _mainFrame.Navigate (page);
         }
     }
 }
