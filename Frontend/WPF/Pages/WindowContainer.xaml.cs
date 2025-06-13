b85c5ae5e9d65dc319c856cdd431e1f0816b74ff
x3776x <abrahamvvquinto@gmail.com>
2025-06-12 22:27:17 -0600

    add/fix NavigationManager, Login window.

--- Frontend/WPF/Pages/WindowContainer.xaml.cs
+++ Frontend/WPF/Pages/WindowContainer.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using WpfApp.Pages.User.Auth;
 using WpfApp.Services.User.Auth;
+using WpfApp.Utilities;
 
 namespace WpfApp.Pages {
     public partial class WindowContainer : Window {
         public WindowContainer () {
             InitializeComponent ();
-            //MainFrame.Navigate (new Login (_loginService));
+            NavigationManager.Initialize(MainFrame);
         }
 
         private void BtnHome_Click(object sender, RoutedEventArgs e)
@@ -15,6 +16,36 @@ namespace WpfApp.Pages {
              * Reload Index on click
              * Create NavigationManager
              */
+        }
+
+        private void BtnCategories_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnOffers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnProfile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnShoppingCart_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
