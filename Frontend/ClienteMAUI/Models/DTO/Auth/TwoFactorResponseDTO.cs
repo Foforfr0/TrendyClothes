cd23c98584f332a7af9fb64d5b20c61128a87a68
FerdyReVive <ferdyrevive@hotmail.com>
2025-06-30 18:52:57 -0600

    login y crear cuenta ya tip top chupete de dedo

---
+++ Frontend/ClienteMAUI/Models/DTO/Auth/TwoFactorResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteMAUI.Models.DTO.Auth
+{
+    public class TwoFactorResponseDTO
+    {
+        public string JwtToken { get; set; } = string.Empty;
+    }
+}
