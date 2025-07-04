1d3c98e0429d14a637cb63b78c2f539a87788697
Foforfr0 <foforfr007@gmail.com>
2025-07-04 05:43:55 -0600

    WPF con gRPC en perfil principal

--- Frontend/WPF/Components/ItemCard2.xaml.cs
+++ Frontend/WPF/Components/ItemCard2.xaml.cs
@@ -1,41 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WpfApp.Pages.Auction;
 using WpfApp.Pages.Product;
 using WpfApp.Utilities;
 
-namespace WpfApp.Components
-{
-    public partial class ItemCard2 : UserControl
-    {
+namespace WpfApp.Components {
+    public partial class ItemCard2 : UserControl {
         public bool isSelectable { get; set; } = false;
         public event EventHandler CardSelected;
-        public ItemCard2()
-        {
-            InitializeComponent();
+        public ItemCard2 () {
+            InitializeComponent ();
         }
 
-        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (isSelectable)
-            {
-                CardSelected?.Invoke(this, EventArgs.Empty);
-            }
-            else
-            {
-                NavigationManager.Instance.NavigateToPage("Product_Header", new ProductDetailsPage());
+        private void Border_MouseLeftButtonDown (object sender, MouseButtonEventArgs e) {
+            if (isSelectable) {
+                CardSelected?.Invoke (this, EventArgs.Empty);
+            } else {
+                NavigationManager.Instance.NavigateToPage ("Product_Header", new ProductDetailsPage ());
             }
         }
     }
--- Frontend/WPF/Pages/Home/HomePage.xaml.cs
+++ Frontend/WPF/Pages/Home/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.Json.Serialization;
 using System.Windows.Controls;
 using WebPage.Connections;
@@ -7,63 +8,51 @@ using WpfApp.DTO.Products;
 using WpfApp.Pages.Dialogs;
 using WpfApp.Utilities;
 
-namespace WpfApp.Pages.Home
-{
-    public partial class HomePage : Page
-    {
-        public HomePage()
-        {
-            InitializeComponent();
-            LoadCategoryCards();
+namespace WpfApp.Pages.Home {
+    public partial class HomePage : Page {
+        public HomePage () {
+            InitializeComponent ();
+            LoadCategoryCards ();
         }
 
-        public async void LoadCategoryCards()
-        {
-            try
-            {
-                var response = await HttpClientHelper.GetAsync(ProductEndpoints.GetCategories);
+        public async void LoadCategoryCards () {
+            try {
+                HttpResponseMessage? response = await HttpClientHelper.GetAsync (ProductEndpoints.GetCategories);
 
-                if (!response.IsSuccessStatusCode)
-                {
-                    MessageDialog.Show("Error", "No se pudieron cargar las categorías", AlertType.ERROR);
+                if (!response.IsSuccessStatusCode) {
+                    MessageDialog.Show ("Error", "No se pudieron cargar las categorías", AlertType.ERROR);
                     return;
                 }
 
-                var result = await response.Content.ReadFromJsonAsync<CategoryResponse>();
+                CategoryResponse? result = await response.Content.ReadFromJsonAsync<CategoryResponse> ();
 
-                if (result?.Body is null || result.Body.Count == 0)
-                {
-                    MessageDialog.Show("Info", "No hay categorías disponibles", AlertType.WARNING);
+                if (result?.Body is null || result.Body.Count == 0) {
+                    MessageDialog.Show ("Info", "No hay categorías disponibles", AlertType.WARNING);
                     return;
                 }
 
                 // Map categories to cards
-                var categoryCards = result.Body.Select(cat =>
-                {
-                    var card = new CategorieCard2();
+                List<CategorieCard2>? categoryCards = result.Body.Select (cat => {
+                    CategorieCard2? card = new CategorieCard2 ();
                     card.BtnCategory.Content = cat.Category;
                     return card;
-                }).ToList();
+                }).ToList ();
 
                 ItemFeed.ItemsSource = categoryCards;
-            }
-            catch (Exception ex)
-            {
-                MessageDialog.Show("Excepción", ex.Message, AlertType.ERROR);
+            } catch (Exception ex) {
+                MessageDialog.Show ("Excepción", ex.Message, AlertType.ERROR);
             }
         }
 
-        public class CategoryResponse
-        {
-            [JsonPropertyName("message")]
+        public class CategoryResponse {
+            [JsonPropertyName ("message")]
             public string Message { get; set; } = "";
 
-            [JsonPropertyName("body")]
-            public List<CategoryDTO> Body { get; set; } = new();
+            [JsonPropertyName ("body")]
+            public List<CategoryDTO> Body { get; set; } = new ();
         }
 
-        private void CardSelected(object sender, EventArgs e)
-        {
+        private void CardSelected (object sender, EventArgs e) {
 
         }
     }
