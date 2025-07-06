146317fc9f662d414556c46d26c7685aaeb8203f
Foforfr0 <foforfr007@gmail.com>
2025-07-06 00:24:41 -0600

    Hola ferdy

--- Frontend/ASP UI/Pages/Auction/Auctioneer/CreateAuction.cshtml.cs
+++ Frontend/ASP UI/Pages/Auction/Auctioneer/CreateAuction.cshtml.cs
@@ -8,14 +8,7 @@ using WebPage.DTO.Auction;
 
 namespace WebPage.Pages.Auction.Auctioneer {
     public class CreateAuctionModel : PageModel {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly ServicesBuilder _services;
-        private readonly GetImageService.GetImageServiceClient _grpcClient;
-
-        public CreateAuctionModel (IHttpClientFactory httpClientFactory, ServicesBuilder services, GetImageService.GetImageServiceClient grpcClient) {
-            _httpClientFactory = httpClientFactory;
-            _services = services;
-            _grpcClient = grpcClient;
+        public CreateAuctionModel () {
         }
 
         [BindProperty]
@@ -23,25 +16,7 @@ namespace WebPage.Pages.Auction.Auctioneer {
             get; set;
         } = new CreateAuctionDTO ();
 
-        public List<StatusAuctionDTO>? statuses;
-        public SelectList? statusSelectList;
-
-        public async Task OnGetAsync () {
-            await InitializeStatussesList ();
-        }
-
-        public async Task InitializeStatussesList () {
-            statuses = new List<StatusAuctionDTO> ();
-
-            HttpClient? httpClient = _httpClientFactory.CreateClient ();
-            string requestURL = "http://apigateway/api/Auction/Statuses";
-            ApiResponse<List<StatusAuctionDTO>>? response =
-                await httpClient.GetFromJsonAsync<ApiResponse<List<StatusAuctionDTO>>> (requestURL);
-
-            if (response?.body != null) {
-                statuses = response.body;
-                statusSelectList = new SelectList (statuses, "Id", "Status");
-            }
+        public void OnGet () {
         }
     }
 }
